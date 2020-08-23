9b9f5ff4affe01e4a8e1d548d626e3466077cc82
Arnaud Clarat <46763863+ArnaudClarat@users.noreply.github.com>
2020-08-23 19:26:02 +0200

    Test

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE)
+{
+    session_start();
+}
+spl_autoload_register(
+    static function($class)
+    {
+        if($class === 'Router') {
+            include 'router/Router.php';
+        } else if (strpos($class, 'Controller')) {
+            include 'controllers/{$class}.php';
+        } else if (strpos($class, 'View')) {
+            include 'views/{$class}.php';
+        } else if (strpos($class, 'Behaviour')) {
+            include 'models/dao/{$class}.php';
+        } else if (strpos($class, 'DAO') || strpos($class, 'DAO') === 0) {
+            include 'models/dao/{$class}.php';
+        } else if (strpos($class, 'Helper') || strpos($class, 'Helper') === 0) {
+            include 'helpers/{$class}.php';
+        } else {
+            include 'models/entities/{$class}.php';
+        }
+    }
+);
+
+$router = new Router($_GET, $_POST, $_SERVER['PHP_SELF'], $_SERVER['REQUEST_URI']);
---
+++ router/Router.php
@@ -0,0 +1,69 @@
+<?php
+include ('helpers/SecurityHelper.php');
+
+class Router {
+    private $get;
+    private $post;
+    private $route;
+    private $root;
+    private $controller_list;
+    private $anonymous_list;
+    private $controller;
+    private $controller_name;
+
+    public function __construct($get, $post, $self, $url) {
+        $this->get = $get;
+        $this->post = $post;
+        $this->controller_list = [''];
+        $this->controller_name = false;
+        $this->anonymous_list = ['login', 'CreateUser'];
+        $this->controller = false;
+        $this->root = $this->parseRoot($self);
+        $this->route = $this->parseURL($url);
+        $this->run();
+    }
+
+    private function parseRoot($self) {
+        return str_replace('index.php', '', $self);
+    }
+
+    private function parseURL($url) {
+        $path = str_replace($this->root, '', $url);
+        $path = explode('/', $path);
+        if($path && $path[0]) {
+            if(strpos($path[0],"?") !== false){
+                $path[0]=substr($path[0],0,strpos($path[0],"?"));
+            }
+        }
+        $controller = false;
+        if($path && count($path) && strlen($path[0])) {
+            if($path[0] === 'logout'){
+                session_destroy();
+                header('Location: login');
+            }
+            if (SecurityHelper::getSession()) {
+                $controller = $path[0];
+            } else {
+                $controller = 'login';
+            }
+        } else if(count($path) && !strlen($path[0])) {
+            if (SecurityHelper::getSession()) {
+                $controller = 'ListCommunications';
+            } else {
+                $controller = 'login';
+            }
+
+        }
+        if($controller && in_array($controller, $this->controller_list, true)) {
+            $this->controller_name = ucfirst($controller.'Controller');
+        }
+        return $path;
+
+    }
+
+    private function run() {
+        if($this->controller_name) {
+            $this->controller = new $this->controller_name($this->get, $this->post, $this->route);
+        }
+    }
+}
